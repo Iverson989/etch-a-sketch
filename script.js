362fa5351db3984debef443da33d33656fa86776
Ben Iverson <2513iverson989@gmail.com>
2021-04-28 13:29:17 -0500

    Grid now lays out correctly

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Input is number, Output is that number of divs added to gridDiv
 function createGrid(inputNum) {
   // loop runs inputNum times
-  for (var i = 0; i < inputNum; i++) {
+  for (var i = 0; i < inputNum * inputNum; i++) {
     // for each loop, create a div
     const createDiv = document.createElement("div");
     // assign class pixels to div
@@ -9,5 +9,13 @@ function createGrid(inputNum) {
     // append div to gridDiv
     document.getElementById("grid-div").appendChild(createDiv);
   }
+  layoutSquare(inputNum);
   return;
+}
+
+const gridDiv = document.getElementById("grid-div");
+
+function layoutSquare(inputNum) {
+  gridDiv.style.gridTemplateColumns = `repeat(${inputNum}, 1fr)`;
+  gridDiv.style.gridTemplateRows = `repeat(${inputNum}, 1fr)`;
 }
