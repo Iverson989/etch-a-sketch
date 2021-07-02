f692a95f3a8e7a818b7661c804a8c07a2f36a6aa
Ben Iverson <2513iverson989@gmail.com>
2021-07-02 12:38:52 -0500

    reset button operational, buttons styled, controls div added to contain buttons

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 // gives easy access to parent element
 const gridDiv = document.getElementById("grid-div");
-const allPixels = document.getElementsByClassName("pixels");
 
 // Input is same as createGrid, Output is a square dislay scheme
 function layoutSquare(inputNum) {
   gridDiv.style.gridTemplateColumns = `repeat(${inputNum}, 1fr)`;
   gridDiv.style.gridTemplateRows = `repeat(${inputNum}, 1fr)`;
+}
+
+// add listeners function with color change
+function addListeners() {
+  const pixels = document.querySelectorAll(".pixels");
+  pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseenter", (pixels) => {
+      pixel.style.backgroundColor = "black";
+    });
+  });
 }
 
 // Input is number, Output is that number of divs added to gridDiv
@@ -24,26 +33,26 @@ function createGrid(inputNum) {
   }
   // make square shape of rows/columns
   layoutSquare(inputNum);
+  addListeners();
   return;
 }
 
 // set initial 16X16 grid
 createGrid(16);
 
-// add listeners function with color change
-const pixels = document.querySelectorAll(".pixels");
-pixels.forEach((pixel) => {
-  pixel.addEventListener("mouseenter", (pixels) => {
-    pixel.style.backgroundColor = "black";
-  });
-});
-
 // reset function
+function reset() {
+  const pixels = document.querySelectorAll(".pixels");
+  pixels.forEach((pixel) => {
+    pixel.remove();
+  });
+}
 
 // add grid sizing/reset button
 function selectSize() {
+  reset();
   var size = prompt("Number of pixels per side?", "1-100");
-  if (typeof size != "number" && size <= 100 && size > 0) {
+  if (typeof size != "number" && size > 0) {
     createGrid(size);
   } else {
     var size2 = prompt(
